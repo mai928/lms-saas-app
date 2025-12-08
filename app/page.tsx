718423957f5d0b25e40ee9a87332ff3b6209cb87
maimohammed928 <maimohammed928@gmail.com>
2025-12-08 19:11:38 +0200

    add routes

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>welcome to my saas app</h1>
+      <Button>Let{"'"}s get Started</Button>
+    </div>
+
+  )
+}
+
+export default Page
